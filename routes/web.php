7f359ddc526fd7fb88090f10578abc242346c5d4
Isaac Kuehnle-Nelson <ikethepike@outlook.com>
2019-04-16 08:11:37 +0200

    Updated for clarity and demonstration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,36 @@
 <?php
 
-Route::resource('authors', 'AuthorController');
+// Resource Routes for author
+// -- Return a listing of all authors
+Route::get('authors', 'AuthorController@index');
 
-Route::resource('posts', 'PostController');
+// -- Return a specific author
+Route::get('authors/{id}', 'AuthorController@show');
 
-Route::put('posts/upvote', 'PostController@upvote');
+// -- Create a new author
+Route::post('authors', 'AuthorController@store');
+
+// -- Update an existing author
+Route::put('authors/{id}', 'AuthorController@update');
+
+// -- Delete an author
+Route::delete('authors/{id}', 'AuthorController@delete');
+
+// Resource Routes for author
+// -- Return a listing of all posts
+Route::get('posts', 'PostController@index');
+
+// -- Return a specific post
+Route::get('posts/{id}', 'PostController@show');
+
+// -- Create a new post
+Route::post('posts', 'PostController@store');
+
+// -- Update an existing post
+Route::put('posts/{id}', 'PostController@update');
+
+// -- Delete an post
+Route::delete('posts/{id}', 'PostController@delete');
+
+// -- Upvote a post
+Route::put('posts/upvote/{id}', 'PostController@upvote');
